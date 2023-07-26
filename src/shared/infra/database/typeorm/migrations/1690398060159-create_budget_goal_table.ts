d7d6450aba5266d0b0a86f2d908064ba9ed1d7b7
shimarrudz <vic.shima.vss@gmail.com>
2023-07-26 18:40:43 -0300

    feat: creating budget goal migration

--- src/shared/infra/database/typeorm/migrations/1690398060159-create_budget_goal_table.ts
+++ src/shared/infra/database/typeorm/migrations/1690398060159-create_budget_goal_table.ts
@@ -1,11 +1,49 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateBudgetGoalTable1690398060159 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'banks',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'picture',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'NOW()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'NOW()',
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            isNullable: true,
+            default: null,
+          },
+        ],
+      }),
+      true,
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('banks');
+  }
 }
