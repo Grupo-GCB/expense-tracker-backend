4bea95d5630892a277d20a2a0fd5638800f01edc
shimarrudz <vic.shima.vss@gmail.com>
2023-07-26 16:01:41 -0300

    feat: migrations generateds

---
+++ src/shared/infra/database/typeorm/migrations/1690398045482-create_bank_table.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateBankTable1690398045482 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
