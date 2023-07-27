0a80a7a90105d94e7699ebea410b03f0ed43cf2c
shimarrudz <vic.shima.vss@gmail.com>
2023-07-27 15:42:29 -0300

    refactor: refactoring by diogo's review

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
