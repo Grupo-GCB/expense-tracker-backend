718ac36c399747e629daf58d764d6a7b81561bb9
Diogo-gallina <diogogallina20@gmail.com>
2023-07-28 08:48:28 -0300

    feat: add message in saveUserDTO

--- src/user/dtos/save-user-dto.ts
+++ src/user/dtos/save-user-dto.ts
@@ -1,16 +1,16 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SaveUserDTO {
-  @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Necessário informar o id.' })
+  @IsString({ message: 'Id deve ser uma string.' })
   id: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Necessário informar o nome.' })
+  @IsString({ message: 'Nome deve ser uma string.' })
   name: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @IsEmail()
+  @IsNotEmpty({ message: 'Necessário informar o e-mail.' })
+  @IsString({ message: 'Email deve ser uma string.' })
+  @IsEmail({}, { message: 'Deve ser e-mail válido.' })
   email: string;
 }
