1fdae80c76d221df43eb80c08e3b7813cc345d0b
shimarrudz <vic.shima.vss@gmail.com>
2023-07-28 18:24:47 -0300

    feat: refactoring tests

--- src/user/use-cases/list-user/list-user.spec.ts
+++ src/user/use-cases/list-user/list-user.spec.ts
@@ -11,7 +11,7 @@ describe('Get User', () => {
 
   const user_id = '123456';
 
-  beforeEach(() => {
+  beforeAll(() => {
     userRepository = {
       findById: jest.fn(),
     } as jest.Mocked<IUserRepository>;
@@ -19,44 +19,33 @@ describe('Get User', () => {
     listUserUseCase = new ListUserUseCase(userRepository);
   });
 
-  it('should be able to list a user by user id', async () => {
-    const fakeUser: User = {
+  it('should be able to return user when found', async () => {
+    const user: User = {
       id: user_id,
       name: 'John Doe',
       email: 'johndoe@example.com',
       created_at: new Date(),
     };
 
-    userRepository.findById.mockResolvedValue(fakeUser);
+    userRepository.findById.mockResolvedValue(user);
+
+    const result = await listUserUseCase.execute(user_id);
+
+    expect(result).toEqual(user);
+    expect(userRepository.findById).toHaveBeenCalledWith(user_id);
   });
 
   it('should be able to throw HttpException when user is not found', async () => {
     userRepository.findById.mockResolvedValue(null);
 
-    const promise = listUserUseCase.execute('non_existent_user_id');
-
-    await expect(promise).rejects.toThrow(
+    // Utilizando a função de retorno de erro do expect
+    await expect(async () => {
+      await listUserUseCase.execute('non_existent_user_id');
+    }).rejects.toThrow(
       new HttpException(
         HttpExceptionConstants.USER_NOT_FOUND.message,
         HttpStatus.NOT_FOUND,
       ),
     );
-  });
-
-  it('should be able to return user when found', async () => {
-    const fakeUser: User = {
-      id: user_id,
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      created_at: new Date(),
-    };
-
-    userRepository.findById.mockResolvedValue(fakeUser);
-
-    const result = await listUserUseCase.execute(user_id);
-
-    expect(result).toEqual(fakeUser);
-
-    expect(userRepository.findById).toHaveBeenCalledWith(user_id);
   });
 });
