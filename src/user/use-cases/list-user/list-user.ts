954e6b20f8492700a5976e18956f48fe35999ea9
shimarrudz <vic.shima.vss@gmail.com>
2023-07-28 14:14:23 -0300

    feat: creating list user use case

--- src/user/use-cases/list-user/list-user.ts
+++ src/user/use-cases/list-user/list-user.ts
@@ -1,3 +1,23 @@
-import { User } from "@/user/infra/entities";
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
-import { IUserRepository } from "@/user/infra/interfaces";
+import { User } from '@/user/infra/entities';
+import { IUserRepository } from '@/user/infra/interfaces';
+import { HttpExceptionConstants } from '@/shared/constants/http-exception.constants';
+
+@Injectable()
+export class ListUserUseCase {
+  constructor(private readonly userRepository: IUserRepository) {}
+
+  async execute(user_id: string): Promise<User> {
+    const user = await this.userRepository.findById(user_id);
+
+    if (!user) {
+      throw new HttpException(
+        HttpExceptionConstants.USER_NOT_FOUND.message,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return user;
+  }
+}
