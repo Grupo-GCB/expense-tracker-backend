5de04402a6d771b9a165e869530d14103b91e972
shimarrudz <vic.shima.vss@gmail.com>
2023-07-28 16:41:46 -0300

    refactor: refactoring user.module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from '@/user/infra/entities';
+import { ListUserUseCase } from '@/user/use-cases';
+import { IUserRepository } from '@/user/infra/interfaces';
+import { UserRepository } from '@/user/infra/repositories';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [],
+  providers: [
+    ListUserUseCase,
+    {
+      provide: IUserRepository,
+      useClass: UserRepository,
+    },
+  ],
 })
 export class UserModule {}
