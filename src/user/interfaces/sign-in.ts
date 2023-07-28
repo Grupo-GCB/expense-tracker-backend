6c89e6fe3a34c3e6db52dbc5471a9ecb900ced2f
Diogo-gallina <diogogallina20@gmail.com>
2023-07-28 10:32:17 -0300

    feat: create sign-in interfaces

---
+++ src/user/interfaces/sign-in.ts
@@ -0,0 +1,19 @@
+interface IUserToken {
+  nickname: string;
+  name: string;
+  picture: string;
+  updated_at: Date;
+  email: string;
+  email_virified: boolean;
+  iss: string;
+  aud: string;
+  iat: string;
+  exp: string;
+  sub: string;
+  sid: string;
+  nonce: string;
+}
+
+export interface IDecodedTokenPayload {
+  user: IUserToken;
+}
