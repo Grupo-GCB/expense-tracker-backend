4139d416f92e05e4b98f0836b888e5ab588bdfc4
Diogo-gallina <diogogallina20@gmail.com>
2023-07-28 08:13:10 -0300

    feat: create SaveUserDTO

--- src/user/dtos/save-user-dto.ts
+++ src/user/dtos/save-user-dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class SaveUserDTO {
+  @IsString()
+  @IsNotEmpty()
   id: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 }
