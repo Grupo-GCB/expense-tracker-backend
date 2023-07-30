1a3b962dbdf8e3c37a74c6f643bc839873ebd450
Diogo-gallina <diogogallina20@gmail.com>
2023-07-30 16:02:50 -0300

    feat: add swagger doc

--- src/user/dtos/save-user-dto.ts
+++ src/user/dtos/save-user-dto.ts
@@ -1,16 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SaveUserDTO {
   @IsNotEmpty({ message: 'Necessário informar o id.' })
   @IsString({ message: 'Id deve ser uma string.' })
+  @ApiProperty({
+    example: 'oauth2|6546846841681616844168',
+    description: 'Id do usuário',
+    type: 'string',
+    required: true,
+  })
   id: string;
 
   @IsNotEmpty({ message: 'Necessário informar o nome.' })
   @IsString({ message: 'Nome deve ser uma string.' })
+  @ApiProperty({
+    example: 'John Doe',
+    description: 'Nome do usuário',
+    type: 'string',
+    required: true,
+  })
   name: string;
 
   @IsNotEmpty({ message: 'Necessário informar o e-mail.' })
   @IsString({ message: 'Email deve ser uma string.' })
   @IsEmail({}, { message: 'Deve ser e-mail válido.' })
+  @ApiProperty({
+    example: 'john.doe@example.com',
+    description: 'Email do usuário',
+    type: 'string',
+    required: true,
+  })
   email: string;
 }
