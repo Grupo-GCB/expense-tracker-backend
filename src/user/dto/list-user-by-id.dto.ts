8447811bd204f6a3d2b035ffda741d0584db8d69
shimarrudz <vic.shima.vss@gmail.com>
2023-07-31 19:21:41 -0300

    refactor: dtos

--- src/user/dto/list-user-by-id.dto.ts
+++ src/user/dto/list-user-by-id.dto.ts
@@ -2,15 +2,14 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class ListUserDTO {
-  @IsNotEmpty({ message: 'Necessário informar o id' })
-  @IsString({ message: 'Id deve ser uma string' })
+  @IsNotEmpty({ message: 'Necessário informar o id.' })
+  @IsString({ message: 'Id deve ser uma string.' })
   @Matches(/^[a-zA-Z0-9|-]+$/, {
-    message:
-      'Id do usuário inválido. Deve conter apenas letras, números e traços.',
+    message: 'Id deve conter apenas letras, números e traços.',
   })
   @ApiProperty({
     example: 'google-oauth2|456734566205483104315',
-    description: 'Id do usuário',
+    description: 'Id do usuário.',
     type: 'string',
     required: true,
   })
