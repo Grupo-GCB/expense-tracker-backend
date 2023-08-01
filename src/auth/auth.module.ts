eadcbf956ff12c56234312af1f2fdc7d02204927
Diogo-gallina <diogogallina20@gmail.com>
2023-08-01 17:15:25 -0300

    feat: create auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+
+import { JwtAuthProvider } from './providers/jwt-auth-provider/jwt-auth-provider';
+import { IAuthProvider } from './interfaces';
+
+@Module({
+  providers: [
+    JwtAuthProvider,
+    { provide: IAuthProvider, useClass: JwtAuthProvider },
+  ],
+  exports: [JwtAuthProvider],
+})
+export class AuthModule {}
