15f86f732f4528009c8967bb7284df487d033d80
Diogo-gallina <diogogallina20@gmail.com>
2023-08-01 17:18:59 -0300

    feat: create auth provider

---
+++ src/auth/providers/jwt-auth-provider/jwt-auth-provider.ts
@@ -0,0 +1,46 @@
+import { SaveUserDTO } from '@/user/dto';
+import { JwtService } from '@nestjs/jwt';
+import { UnauthorizedException } from '@nestjs/common';
+import * as jwksRsa from 'jwks-rsa';
+
+import { IDecodedTokenPayload } from '@/user/interfaces';
+import { IAuthProvider } from '@/auth/interfaces';
+
+export class JwtAuthProvider extends JwtService implements IAuthProvider {
+  async decodeToken(jwtToken: string): Promise<SaveUserDTO> {
+    try {
+      const decodedHeader = this.decode(jwtToken, {
+        complete: true,
+      });
+
+      if (typeof decodedHeader === 'string') {
+        throw new UnauthorizedException('Cabeçalho de token inválido.');
+      }
+
+      const jwksClient = jwksRsa({
+        jwksUri: 'https://gcb-academy.us.auth0.com/.well-known/jwks.json',
+      });
+
+      const signingKey = await jwksClient.getSigningKey(
+        decodedHeader.header.kid,
+      );
+      const publicKey = signingKey.getPublicKey();
+
+      const { sub, name, email } = await this.verifyAsync<IDecodedTokenPayload>(
+        jwtToken,
+        {
+          publicKey,
+          algorithms: ['RS256'],
+        },
+      );
+
+      return {
+        id: sub,
+        name,
+        email,
+      };
+    } catch {
+      throw new UnauthorizedException('Token inválido.');
+    }
+  }
+}
