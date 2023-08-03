94c4640d88974de872048f3b5074a5f547f8a681
Diogo-gallina <diogogallina20@gmail.com>
2023-08-03 17:43:19 -0300

    refactor: change method order

--- src/auth/providers/jwt-auth-provider/jwt-auth-provider.ts
+++ src/auth/providers/jwt-auth-provider/jwt-auth-provider.ts
@@ -6,26 +6,6 @@ import { IDecodedTokenPayload } from '@/user/interfaces';
 import { IAuthProvider, IJwtHeader } from '@/auth/interfaces';
 
 export class JwtAuthProvider extends JwtService implements IAuthProvider {
-  private async getPublicKey(kid: string): Promise<string> {
-    const jwksClient = jwksRsa({
-      jwksUri: process.env.JWKS_URI,
-    });
-
-    return (await jwksClient.getSigningKey(kid)).getPublicKey();
-  }
-
-  private decodeJwtHeader(jwtToken: string): IJwtHeader {
-    const decodedHeader = this.decode(jwtToken, {
-      complete: true,
-    }) as IJwtHeader;
-
-    if (!decodedHeader?.header?.kid) {
-      throw new Error('Cabeçalho de token inválido.');
-    }
-
-    return decodedHeader;
-  }
-
   async decodeToken(jwtToken: string): Promise<SaveUserDTO> {
     const decodedHeader = this.decodeJwtHeader(jwtToken);
     const publicKey = await this.getPublicKey(decodedHeader.header.kid);
@@ -42,5 +22,27 @@ export class JwtAuthProvider extends JwtService implements IAuthProvider {
       name,
       email,
     };
+  }
+
+  private decodeJwtHeader(jwtToken: string): IJwtHeader {
+    const decodedHeader = this.decode(jwtToken, {
+      complete: true,
+    }) as IJwtHeader;
+
+    if (!decodedHeader?.header?.kid) {
+      throw new Error('Cabeçalho de token inválido.');
+    }
+
+    return decodedHeader;
+  }
+
+  private async getPublicKey(kid: string): Promise<string> {
+    const jwksClient = jwksRsa({
+      jwksUri: process.env.JWKS_URI,
+    });
+
+    const signInKey = await jwksClient.getSigningKey(kid);
+
+    return signInKey.getPublicKey();
   }
 }
