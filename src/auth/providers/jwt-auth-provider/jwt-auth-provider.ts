144ab930ae13d2704132560a40b8c99acb292cfc
Diogo-gallina <diogogallina20@gmail.com>
2023-08-03 00:06:38 -0300

    refactor: separating responsibility from methods

--- src/auth/providers/jwt-auth-provider/jwt-auth-provider.ts
+++ src/auth/providers/jwt-auth-provider/jwt-auth-provider.ts
@@ -3,23 +3,34 @@ import { JwtService } from '@nestjs/jwt';
 import * as jwksRsa from 'jwks-rsa';
 
 import { IDecodedTokenPayload } from '@/user/interfaces';
-import { IAuthProvider } from '@/auth/interfaces';
+import { IAuthProvider, IJwtHeader } from '@/auth/interfaces';
 
 export class JwtAuthProvider extends JwtService implements IAuthProvider {
-  async decodeToken(jwtToken: string): Promise<SaveUserDTO> {
-    const decodedHeader = this.decode(jwtToken, {
-      complete: true,
-    });
-
-    if (typeof decodedHeader === 'string')
-      throw new Error('Cabeçalho de token inválido.');
-
+  private async getPublicKey(kid: string): Promise<string> {
     const jwksClient = jwksRsa({
       jwksUri: process.env.JWKS_URI,
     });
 
-    const signingKey = await jwksClient.getSigningKey(decodedHeader.header.kid);
+    const signingKey = await jwksClient.getSigningKey(kid);
     const publicKey = signingKey.getPublicKey();
+    return publicKey;
+  }
+
+  private decodeJwtHeader(jwtToken: string): IJwtHeader {
+    const decodedHeader = this.decode(jwtToken, {
+      complete: true,
+    }) as IJwtHeader;
+
+    if (!decodedHeader || !decodedHeader.header || !decodedHeader.header.kid) {
+      throw new Error('Cabeçalho de token inválido.');
+    }
+
+    return decodedHeader;
+  }
+
+  async decodeToken(jwtToken: string): Promise<SaveUserDTO> {
+    const decodedHeader = this.decodeJwtHeader(jwtToken);
+    const publicKey = await this.getPublicKey(decodedHeader.header.kid);
 
     const { sub, name, email } = await this.verifyAsync<IDecodedTokenPayload>(
       jwtToken,
