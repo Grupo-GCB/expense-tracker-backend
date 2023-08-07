4750d29af319bbbd262df908ce981c74102bafb1
Diogo-gallina <diogogallina20@gmail.com>
2023-08-07 15:43:35 -0300

    refactor: optimizing code

--- src/shared/utils/database-config.ts
+++ src/shared/utils/database-config.ts
@@ -3,24 +3,14 @@ import 'dotenv/config';
 import { IDatabaseConfig } from '@/shared/interfaces';
 
 const env = process.env.NODE_ENV;
-let databaseConfig: IDatabaseConfig;
+const envPrefix = env === 'prod' ? '' : '_DEV';
 
-if (env === 'prod') {
-  databaseConfig = {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  };
-} else if (env === 'dev') {
-  databaseConfig = {
-    host: process.env.DB_HOST_DEV,
-    port: Number(process.env.DB_PORT_DEV),
-    username: process.env.DB_USER_DEV,
-    password: process.env.DB_PASSWORD_DEV,
-    database: process.env.DB_NAME_DEV,
-  };
-}
+const databaseConfig: IDatabaseConfig = {
+  host: process.env[`DB_HOST${envPrefix}`],
+  port: Number(process.env[`DB_PORT${envPrefix}`]),
+  username: process.env[`DB_USER${envPrefix}`],
+  password: process.env[`DB_PASSWORD${envPrefix}`],
+  database: process.env[`DB_NAME${envPrefix}`],
+};
 
 export default databaseConfig;
