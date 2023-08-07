dba93562ac18cb9447a2d4ac4370a4c0cbb0c9b7
Diogo-gallina <diogogallina20@gmail.com>
2023-08-07 14:28:54 -0300

    refactor: change environments variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
 
 import { BankModule } from '@/bank/bank.module';
 import { BudgetGoal } from '@/budget-goal/infra/entities';
 import { Transaction } from '@/transaction/infra/entities';
 import { UserModule } from '@/user/user.module';
 import { Wallet } from '@/wallet/infra/entities';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from '@/auth/auth.module';
+import databaseConfig from '@/shared/ultils/database-config';
 
 @Module({
   imports: [
@@ -16,11 +18,11 @@ import { AuthModule } from './auth/auth.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      port: Number(process.env.DB_PORT),
+      host: databaseConfig.host,
+      port: databaseConfig.port,
+      username: databaseConfig.username,
+      password: databaseConfig.password,
+      database: databaseConfig.database,
       entities: [`${__dirname}/**/*.entity{.js,.ts}`],
       migrations: [
         `${__dirname}/shared/infra/database/typeorm/migrations/{.ts,*.js}`,
