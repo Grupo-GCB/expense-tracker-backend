d85c070c3c51b29510153b2e770a414b0657d22b
shimarrudz <vic.shima.vss@gmail.com>
2023-08-07 17:43:45 -0300

    feat: add swagger on DTO

--- src/wallet/dto/save-wallet-dto.ts
+++ src/wallet/dto/save-wallet-dto.ts
@@ -1,21 +1,42 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty } from 'class-validator';
 
 import { AccountType } from '@/shared/constants/enums';
 
 export class SaveWalletDTO {
+  @ApiProperty({
+    example: 'Banco Itáu',
+    description: 'Título da carteira.',
+  })
   @IsNotEmpty({ message: 'Necessário informar o título.' })
   title: string;
 
+  @ApiProperty({
+    enum: AccountType,
+    description: 'Tipo de conta da carteira.',
+  })
   @IsNotEmpty({ message: 'Necessário ser um enum.' })
   @IsEnum(AccountType)
   account_type: AccountType;
 
+  @ApiProperty({
+    example: 'Esta carteira foi criada para viagens.',
+    description: 'Descrição da carteira.',
+  })
   @IsNotEmpty({ message: 'Necessário informar a descrição.' })
   description: string;
 
+  @ApiProperty({
+    example: '4e8b5d94-6b16-4a42-b6d1-dc58b553d109',
+    description: 'ID do banco associado à carteira.',
+  })
   @IsNotEmpty({ message: 'Necessário informar o id do banco.' })
   bankId: string;
 
+  @ApiProperty({
+    example: 'auth0|58vfb567d5asdea52bc65ebba',
+    description: 'ID do usuário proprietário da carteira.',
+  })
   @IsNotEmpty({ message: 'Necessário informar o id do usuário.' })
   userId: string;
 }
