d84de16deeb7550a54b0d9fa0ce6648c5a93a025
shimarrudz <vic.shima.vss@gmail.com>
2023-08-07 17:36:38 -0300

    feat: add swagger on controller

--- src/shared/infra/http/controllers/wallet/wallet.controller.ts
+++ src/shared/infra/http/controllers/wallet/wallet.controller.ts
@@ -1,14 +1,33 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiOkResponse,
+  ApiBadRequestResponse,
+} from '@nestjs/swagger';
 
 import { RegisterWalletUseCase } from '@/wallet/use-cases';
 import { Wallet } from '@/wallet/infra/entities';
 import { SaveWalletDTO } from '@/wallet/dto';
 
+@ApiTags('Wallet')
 @Controller('wallet')
 export class WalletController {
   constructor(private readonly walletUseCase: RegisterWalletUseCase) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Registrar uma carteira.',
+    description: 'Esta rota permite registrar uma carteira de um usuário.',
+  })
+  @ApiOkResponse({
+    description: 'Carteira registrada com sucesso.',
+    type: Wallet,
+  })
+  @ApiBadRequestResponse({
+    description:
+      'Erro de validação. Veja a mensagem de erro para mais detalhes.',
+  })
   async createWallet(@Body() walletData: SaveWalletDTO): Promise<Wallet> {
     const createdWallet = await this.walletUseCase.createWallet(walletData);
     return createdWallet;
