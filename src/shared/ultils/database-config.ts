036dcedd0d5c48366eab987ed7ba5591ec475c31
Diogo-gallina <diogogallina20@gmail.com>
2023-08-07 14:30:11 -0300

    feat: create database config

---
+++ src/shared/ultils/database-config.ts
@@ -0,0 +1,25 @@
+import { IDatabaseConfig } from '../interfaces';
+import 'dotenv/config';
+
+const env = process.env.NODE_ENV;
+let databaseConfig: IDatabaseConfig;
+
+if (env === 'prod') {
+  databaseConfig = {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  };
+} else if (env === 'dev') {
+  databaseConfig = {
+    host: process.env.DB_HOST_DEV,
+    port: Number(process.env.DB_PORT_DEV),
+    username: process.env.DB_USER_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_NAME_DEV,
+  };
+}
+
+export default databaseConfig;
