2a1d89c17740ea7790d61beebf42886572f858b5
Diogo-gallina <diogogallina20@gmail.com>
2023-08-08 15:54:24 -0300

    chore: remove mock date

--- src/bank/use-cases/find-bank-by-id/find-bank-by-id.spec.ts
+++ src/bank/use-cases/find-bank-by-id/find-bank-by-id.spec.ts
@@ -8,11 +8,8 @@ describe('Find Bank by Id', () => {
   let findBankById: FindBankByIdUseCase;
   let bankRepository: jest.Mocked<IBankRepository>;
 
-  global.Date.now = jest.fn(() => new Date('06/08/2020').getTime());
-
   const bankId = '097d540a-2298-4600-b0f4-77f1e3aasf2';
   const nonExistentBankId = 'non-existent-bank-id';
-  const ceratedAtDateMock = new Date('02/05/2023');
 
   beforeAll(() => {
     bankRepository = {
@@ -27,8 +24,6 @@ describe('Find Bank by Id', () => {
       id: bankId,
       name: 'anyBank',
       logo_url: 'anyURL',
-      created_at: ceratedAtDateMock,
-      deleted_at: null,
     } as Bank;
 
     bankRepository.findById.mockResolvedValue(bank);
