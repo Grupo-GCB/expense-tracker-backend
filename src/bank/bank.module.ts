e1f14f7251c5a3e98b994f814ef0d51d2582455b
Diogo-gallina <diogogallina20@gmail.com>
2023-08-08 14:23:23 -0300

    feat: add controller dependencies

--- src/bank/bank.module.ts
+++ src/bank/bank.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Bank } from '@/bank/infra/entities';
+import { BankController } from '@/shared/infra/http/controllers';
+import { FindBankByIdUseCase } from './use-cases';
+import { IBankRepository } from './interfaces';
+import { BankRepository } from './infra/repositories';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Bank])],
-  controllers: [],
+  controllers: [BankController],
+  providers: [
+    FindBankByIdUseCase,
+    {
+      provide: IBankRepository,
+      useClass: BankRepository,
+    },
+  ],
 })
 export class BankModule {}
