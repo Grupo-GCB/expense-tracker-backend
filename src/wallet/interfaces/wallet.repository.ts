914b071dbce86922ef20f53e63c19596de774635
Diogo-gallina <diogogallina20@gmail.com>
2023-08-10 13:27:15 -0300

    feat: add update method in repository inteface

--- src/wallet/interfaces/wallet.repository.ts
+++ src/wallet/interfaces/wallet.repository.ts
@@ -1,6 +1,8 @@
-import { SaveWalletDTO } from '@/wallet/dto';
+import { SaveWalletDTO, UpdateWalletDTO } from '@/wallet/dto';
 import { Wallet } from '@/wallet/infra/entities';
 
 export abstract class IWalletRepository {
   abstract create(data: SaveWalletDTO): Promise<Wallet>;
+  abstract update(data: UpdateWalletDTO): Promise<Wallet>;
+  abstract findById(wallet_id: string): Promise<Wallet>;
 }
