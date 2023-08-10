9def67d80928888cfca3726c9cd61eb8180542e5
Diogo-gallina <diogogallina20@gmail.com>
2023-08-10 14:04:27 -0300

    feat: create update wallet route

--- src/shared/infra/http/controllers/wallet/wallet.controller.ts
+++ src/shared/infra/http/controllers/wallet/wallet.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Put } from '@nestjs/common';
 import {
   ApiTags,
   ApiOperation,
@@ -7,15 +7,18 @@ import {
   ApiNotFoundResponse,
 } from '@nestjs/swagger';
 
-import { RegisterWalletUseCase } from '@/wallet/use-cases';
+import { RegisterWalletUseCase, UpdateWalletUseCase } from '@/wallet/use-cases';
 import { Wallet } from '@/wallet/infra/entities';
-import { SaveWalletDTO } from '@/wallet/dto';
+import { SaveWalletDTO, UpdateWalletDTO } from '@/wallet/dto';
 import { API_RESPONSES } from '@/shared/constants';
 
 @ApiTags('Wallet')
 @Controller('wallet')
 export class WalletController {
-  constructor(private readonly walletUseCase: RegisterWalletUseCase) {}
+  constructor(
+    private readonly walletUseCase: RegisterWalletUseCase,
+    private readonly updateWalletUseCase: UpdateWalletUseCase,
+  ) {}
 
   @Post()
   @ApiOperation({
@@ -28,5 +31,17 @@ export class WalletController {
   @ApiNotFoundResponse(API_RESPONSES.NOT_FOUND)
   async createWallet(@Body() walletData: SaveWalletDTO): Promise<Wallet> {
     return this.walletUseCase.createWallet(walletData);
+  }
+
+  @Put('update')
+  @ApiOperation({
+    summary: 'Atualizar uma carteira.',
+    description: 'Esta rota permite atualizar uma carteira de um usuário.',
+  })
+  @ApiOkResponse(API_RESPONSES.OK)
+  @ApiBadRequestResponse(API_RESPONSES.BAD_REQUEST)
+  @ApiNotFoundResponse(API_RESPONSES.NOT_FOUND)
+  async updateWallet(@Body() data: UpdateWalletDTO): Promise<Wallet> {
+    return this.updateWalletUseCase.execute(data);
   }
 }
