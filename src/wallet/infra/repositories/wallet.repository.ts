1d422d7a41350b859b36ae58a53d9be9bb347119
Diogo-gallina <diogogallina20@gmail.com>
2023-08-10 13:26:13 -0300

    feat: create update method

--- src/wallet/infra/repositories/wallet.repository.ts
+++ src/wallet/infra/repositories/wallet.repository.ts
@@ -4,7 +4,7 @@ import { Repository } from 'typeorm';
 
 import { Wallet } from '@/wallet/infra/entities';
 import { IWalletRepository } from '@/wallet/interfaces';
-import { SaveWalletDTO } from '@/wallet/dto';
+import { SaveWalletDTO, UpdateWalletDTO } from '@/wallet/dto';
 
 @Injectable()
 export class WalletRepository implements IWalletRepository {
@@ -15,6 +15,18 @@ export class WalletRepository implements IWalletRepository {
 
   async create(data: SaveWalletDTO): Promise<Wallet> {
     const wallet = this.walletRepository.create(data);
+    return this.walletRepository.save(wallet);
+  }
+
+  async findById(id: string): Promise<Wallet> {
+    return this.walletRepository.findOne({ where: { id } });
+  }
+
+  async update(data: UpdateWalletDTO): Promise<Wallet> {
+    const wallet = await this.findById(data.id);
+
+    Object.assign(wallet, data);
+
     return this.walletRepository.save(wallet);
   }
 }
