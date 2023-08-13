28a2f28e92c8d85663e27be42801094ce7a78cab
shimarrudz <vic.shima.vss@gmail.com>
2023-08-13 19:28:41 -0300

    test: create exception of wallet return test

--- src/wallet/use-cases/find-by-id/find-by-id.spec.ts
+++ src/wallet/use-cases/find-by-id/find-by-id.spec.ts
@@ -2,6 +2,7 @@ import { AccountType } from '@/shared/constants';
 import { Wallet } from '@/wallet/infra/entities';
 import { IWalletRepository } from '@/wallet/interfaces';
 import { FindWalletByIdUseCase } from '@/wallet/use-cases';
+import { NotFoundException } from '@nestjs/common';
 
 describe('Find Bank by ID', () => {
   let findWalletById: FindWalletByIdUseCase;
@@ -38,5 +39,13 @@ describe('Find Bank by ID', () => {
     expect(result.wallet).toEqual(walletData);
     expect(walletRepository.findById).toHaveBeenCalledWith(walletId);
     expect(walletRepository.findById).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not be able to return a wallet', async () => {
+    walletRepository.findById.mockResolvedValueOnce(null);
+
+    await expect(
+      findWalletById.execute(nonExistentWalletId),
+    ).rejects.toBeInstanceOf(NotFoundException);
   });
 });
