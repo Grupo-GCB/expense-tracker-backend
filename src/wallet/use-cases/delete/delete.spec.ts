f8542fbe043f8a05eb9a8ee4848d155b4447d0c1
Diogo-gallina <diogogallina20@gmail.com>
2023-08-18 15:47:05 -0300

    test: create delete sucess test

--- src/wallet/use-cases/delete/delete.spec.ts
+++ src/wallet/use-cases/delete/delete.spec.ts
@@ -1,16 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
-import { DeleteWalletUseCase } from './delete';
 import { IWalletRepository } from '@/wallet/interfaces';
 import { AccountType } from '@/shared/constants';
-import { UpdateWalletDTO } from '@/wallet/dto';
 import { NotFoundException } from '@nestjs/common';
+import { DeleteWalletUseCase } from '@/wallet/use-cases';
+import { Wallet } from '@/wallet/infra/entities';
 
 describe('Delete Wallet Use Case', () => {
   let walletRepository: IWalletRepository;
   let sut: DeleteWalletUseCase;
+  let findByIdMock: jest.SpyInstance;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         DeleteWalletUseCase,
@@ -18,6 +19,7 @@ describe('Delete Wallet Use Case', () => {
           provide: IWalletRepository,
           useFactory: () => ({
             findById: jest.fn(),
+            create: jest.fn(),
             delete: jest.fn(),
           }),
         },
@@ -26,21 +28,35 @@ describe('Delete Wallet Use Case', () => {
 
     sut = module.get<DeleteWalletUseCase>(DeleteWalletUseCase);
     walletRepository = module.get<IWalletRepository>(IWalletRepository);
+
+    findByIdMock = jest.spyOn(walletRepository, 'findById');
   });
 
   const validId = '4e8b5d94-6b16-4a42-b6d1-dc58b553d109';
   const invalidId = 'invalid-id';
 
-  const walletData: UpdateWalletDTO = {
+  const wallet = {
     id: validId,
-    bank_id: validId,
     account_type: AccountType.CHECKING_ACCOUNT,
-    description: 'Nova descrição',
-  };
+    description: 'Descrição',
+    deleted_at: null as Date | null,
+  } as Wallet;
 
-  it.only('should not be able to delete a wallet when non exist wallet', async () => {
-    const nonExistingWalletId: UpdateWalletDTO = {
-      ...walletData,
+  it.only('should be able to delete a wallet when exist a wallet', async () => {
+    findByIdMock.mockResolvedValue(wallet);
+
+    await sut.execute({ id: wallet.id });
+
+    expect(walletRepository.findById).toHaveBeenCalledTimes(1);
+    expect(walletRepository.findById).toHaveBeenCalledWith(wallet.id);
+
+    expect(walletRepository.delete).toHaveBeenCalledTimes(1);
+    expect(walletRepository.delete).toHaveBeenCalledWith(wallet.id);
+  });
+
+  it('should not be able to delete a wallet when non exist wallet', async () => {
+    const nonExistingWalletId: Wallet = {
+      ...wallet,
       id: invalidId,
     };
 
