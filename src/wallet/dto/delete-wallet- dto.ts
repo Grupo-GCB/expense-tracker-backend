00c0fb5acb0509d4264486e3590ad7a85e1a1d01
Diogo-gallina <diogogallina20@gmail.com>
2023-08-18 10:39:27 -0300

    feat: add string validation in DTO

--- src/wallet/dto/delete-wallet- dto.ts
+++ src/wallet/dto/delete-wallet- dto.ts
@@ -1,11 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class DeleteWalletDTO {
   @ApiProperty({
     example: '4e8b5d94-6b16-4a42-b6d1-dc58b553d109',
     description: 'Id da carteira.',
   })
+  @IsString({ message: 'Id deve ser uma string.' })
   @IsNotEmpty({ message: 'Necessário informar o id da carteira.' })
   @IsUUID('all', { message: 'Necessário que o id seja do tipo UUID.' })
   id: string;
