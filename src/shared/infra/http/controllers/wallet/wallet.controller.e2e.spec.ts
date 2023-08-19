5c4981ae7210a70706b2ceaa2ca9a2258d52ba76
shimarrudz <vic.shima.vss@gmail.com>
2023-08-18 23:19:14 -0300

    test: change tests order for a better reading

--- src/shared/infra/http/controllers/wallet/wallet.controller.e2e.spec.ts
+++ src/shared/infra/http/controllers/wallet/wallet.controller.e2e.spec.ts
@@ -87,6 +87,59 @@ describe('Wallet Controller (E2E)', () => {
     await app.close();
   });
 
+  describe('/wallet (POST)', () => {
+    it('should be defined', () => {
+      expect(walletRepository).toBeDefined();
+      expect(createWalletMock).toBeDefined();
+    });
+
+    it('should create a wallet', async () => {
+      createWalletMock.mockResolvedValue(walletData);
+
+      const response = await request(app.getHttpServer())
+        .post('/wallet')
+        .send(walletData)
+        .expect(HttpStatus.CREATED);
+
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          user_id: expect.any(String),
+          bank_id: expect.any(String),
+        }),
+      );
+      expect(response.body.account_type).toBe(AccountType.CHECKING_ACCOUNT);
+      expect(response.body.description).toBe('Descrição da carteira');
+    });
+
+    it('should not be able to register a wallet if user does not exist', async () => {
+      createWalletMock.mockRejectedValue(new NotFoundException());
+
+      const dtoWithNonExistingUser: SaveWalletDTO = {
+        ...walletData,
+        user_id: 'non_existing_user_id',
+      };
+
+      await request(app.getHttpServer())
+        .post('/wallet')
+        .send(dtoWithNonExistingUser)
+        .expect(HttpStatus.NOT_FOUND);
+    });
+
+    it('should not be able to register a wallet if bank does not exist', async () => {
+      createWalletMock.mockRejectedValue(new NotFoundException());
+
+      const dtoWithNonExistingBank: SaveWalletDTO = {
+        ...walletData,
+        bank_id: 'd344a168-60ad-48fc-9d57-64b412e4f6d5',
+      };
+
+      await request(app.getHttpServer())
+        .post('/wallet')
+        .send(dtoWithNonExistingBank)
+        .expect(HttpStatus.NOT_FOUND);
+    });
+  });
+
   describe('/wallets/:id (GET)', () => {
     it('should be able to return a list with all wallets', async () => {
       const wallets = [mockWallet, mockWallet];
@@ -143,59 +196,6 @@ describe('Wallet Controller (E2E)', () => {
 
       await request(app.getHttpServer())
         .get(`/wallet/${nonExistentWalletId}`)
-        .expect(HttpStatus.NOT_FOUND);
-    });
-  });
-
-  describe('/wallet (POST)', () => {
-    it('should be defined', () => {
-      expect(walletRepository).toBeDefined();
-      expect(createWalletMock).toBeDefined();
-    });
-
-    it('should create a wallet', async () => {
-      createWalletMock.mockResolvedValue(walletData);
-
-      const response = await request(app.getHttpServer())
-        .post('/wallet')
-        .send(walletData)
-        .expect(HttpStatus.CREATED);
-
-      expect(response.body).toEqual(
-        expect.objectContaining({
-          user_id: expect.any(String),
-          bank_id: expect.any(String),
-        }),
-      );
-      expect(response.body.account_type).toBe(AccountType.CHECKING_ACCOUNT);
-      expect(response.body.description).toBe('Descrição da carteira');
-    });
-
-    it('should not be able to register a wallet if user does not exist', async () => {
-      createWalletMock.mockRejectedValue(new NotFoundException());
-
-      const dtoWithNonExistingUser: SaveWalletDTO = {
-        ...walletData,
-        user_id: 'non_existing_user_id',
-      };
-
-      await request(app.getHttpServer())
-        .post('/wallet')
-        .send(dtoWithNonExistingUser)
-        .expect(HttpStatus.NOT_FOUND);
-    });
-
-    it('should not be able to register a wallet if bank does not exist', async () => {
-      createWalletMock.mockRejectedValue(new NotFoundException());
-
-      const dtoWithNonExistingBank: SaveWalletDTO = {
-        ...walletData,
-        bank_id: 'd344a168-60ad-48fc-9d57-64b412e4f6d5',
-      };
-
-      await request(app.getHttpServer())
-        .post('/wallet')
-        .send(dtoWithNonExistingBank)
         .expect(HttpStatus.NOT_FOUND);
     });
   });
