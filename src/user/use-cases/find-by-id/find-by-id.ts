1b1340c7932987a5033758fa2388d75830c83a9b
shimarrudz <vic.shima.vss@gmail.com>
2023-08-20 12:22:23 -0300

    build: remove useless import

--- src/user/use-cases/find-by-id/find-by-id.ts
+++ src/user/use-cases/find-by-id/find-by-id.ts
@@ -1,6 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 
-import { User } from '@/user/infra/entities';
 import { IUserRepository, IUserResponse } from '@/user/interfaces';
 
 @Injectable()
