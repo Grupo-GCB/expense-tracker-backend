1e2f3b81fd581ce06cf5d943ff13118279e53a07
shimarrudz <vic.shima.vss@gmail.com>
2023-08-20 22:53:47 -0300

    test: refactor find user by id test and add sut variable name

--- src/user/use-cases/find-by-id/find-by-id.spec.ts
+++ src/user/use-cases/find-by-id/find-by-id.spec.ts
@@ -6,50 +6,50 @@ import { FindUserByIdUseCase } from '@/user/use-cases';
 
 describe('Find User By Id', () => {
   let userRepository: jest.Mocked<IUserRepository>;
-  let findUserUseCase: FindUserByIdUseCase;
+  let sut: FindUserByIdUseCase;
   let findByIdMock: jest.SpyInstance;
-
-  const userId = '123456';
-  const nonExistentUserId = 'non-existent-user-id';
-
-  const user: User = {
-    id: userId,
-    name: 'John Doe',
-    email: 'johndoe@example.com',
-    created_at: new Date(),
-    wallet: [],
-  };
 
   beforeAll(() => {
     userRepository = {
       findById: jest.fn(),
     } as unknown as jest.Mocked<IUserRepository>;
 
-    findUserUseCase = new FindUserByIdUseCase(userRepository);
+    sut = new FindUserByIdUseCase(userRepository);
     findByIdMock = jest.spyOn(userRepository, 'findById');
   });
 
+  const validUserId = '123456';
+  const invalidUserId = 'non-existent-user-id';
+
+  const user: User = {
+    id: validUserId,
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    created_at: new Date(),
+    wallet: [],
+  };
+
   it('should be defined', () => {
     expect(userRepository).toBeDefined();
-    expect(findUserUseCase).toBeDefined();
+    expect(sut).toBeDefined();
     expect(findByIdMock).toBeDefined();
   });
 
   it('should be able to return an user', async () => {
     findByIdMock.mockResolvedValue(user);
 
-    const result = await findUserUseCase.execute(userId);
+    const result = await sut.execute(validUserId);
 
     expect(result.user).toEqual(user);
-    expect(findByIdMock).toHaveBeenCalledWith(userId);
+    expect(findByIdMock).toHaveBeenCalledWith(validUserId);
     expect(findByIdMock).toHaveBeenCalledTimes(1);
   });
 
   it('should not be able to return an user', async () => {
     findByIdMock.mockResolvedValueOnce(null);
 
-    await expect(
-      findUserUseCase.execute(nonExistentUserId),
-    ).rejects.toThrowError(new NotFoundException('Usuário não encontrado.'));
+    await expect(sut.execute(invalidUserId)).rejects.toThrowError(
+      new NotFoundException('Usuário não encontrado.'),
+    );
   });
 });
