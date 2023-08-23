bc4be5d5c3547c02f308ed24bd0d5d8ebad6e5f5
Diogo-gallina <diogogallina20@gmail.com>
2023-08-23 01:45:43 -0300

    feat: create register transaction use case

---
+++ src/transaction/use-cases/register/register.ts
@@ -0,0 +1,27 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { TransactionType } from '@/shared/constants';
+import { CreateTransactionDTO } from '@/transaction/dto';
+import { Transaction } from '@/transaction/infra/entities';
+import { ITransactionRepository } from '@/transaction/interface';
+import { IWalletRepository } from '@/wallet/interfaces';
+
+@Injectable()
+export class RegisterTransactionUseCase {
+  constructor(
+    private readonly transactionRepository: ITransactionRepository,
+    private readonly walletRepository: IWalletRepository,
+  ) {}
+
+  async execute(
+    wallet_id: string,
+    data: CreateTransactionDTO,
+  ): Promise<Transaction> {
+    const wallet = await this.walletRepository.findById(wallet_id);
+    if (!wallet) throw new NotFoundException('Carteira não encontrada.');
+
+    if (data.type === TransactionType.EXPENSE) data.value *= -1;
+
+    return await this.transactionRepository.create(wallet_id, data);
+  }
+}
