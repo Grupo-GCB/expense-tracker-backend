9a6105e6058c43c0361521bac8d2086aabb5e01a
Diogo-gallina <diogogallina20@gmail.com>
2023-08-23 01:42:46 -0300

    config: add injections, providers and imports

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { TransactionController } from '@/shared/infra/http/controllers/transaction/transaction.controller';
 import { Transaction } from '@/transaction/infra/entities';
+import { Wallet } from '@/wallet/infra/entities';
+import { TransactionRepository } from './infra/repositories/transaction-repository';
+import { ITransactionRepository } from './interface';
+import { RegisterTransactionUseCase } from './use-cases/register/register';
+import { WalletRepository } from '@/wallet/infra/repositories';
+import { IWalletRepository } from '@/wallet/interfaces';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction])],
-  controllers: [],
+  imports: [TypeOrmModule.forFeature([Transaction, Wallet])],
+  controllers: [TransactionController],
+  providers: [
+    RegisterTransactionUseCase,
+    {
+      provide: ITransactionRepository,
+      useClass: TransactionRepository,
+    },
+    {
+      provide: IWalletRepository,
+      useClass: WalletRepository,
+    },
+  ],
 })
 export class TransactionModule {}
