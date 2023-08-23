3d99c58bb739c0691710b8dbf1bee7ffa14e6700
Diogo-gallina <diogogallina20@gmail.com>
2023-08-23 19:36:44 -0300

    feat: create enum error message constants

---
+++ src/shared/constants/enum-error-messages.ts
@@ -0,0 +1,8 @@
+export const ENUM_ERROR = {
+  TRANSACTION_TYPE: 'Insira um dos seguintes tipos: Receita, Despesa.',
+
+  CATEGORIES: 'Erro de validação. Veja a mensagem de erro para mais detalhes.',
+
+  ACCOUNT_TYPE:
+    'Insira um dos seguintes tipos: Conta-Corrente, Conta-Poupança, Investimento, Dinheiro ou Outros.',
+};
