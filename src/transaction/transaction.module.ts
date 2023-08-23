883de5bcda390f780d0a023d37fabcc6752983be
Diogo-gallina <diogogallina20@gmail.com>
2023-08-23 10:10:41 -0300

    chore: change imports

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { TransactionController } from '@/shared/infra/http/controllers/transaction/transaction.controller';
+import { TransactionController } from '@/shared/infra/http/controllers/';
 import { Transaction } from '@/transaction/infra/entities';
 import { Wallet } from '@/wallet/infra/entities';
-import { TransactionRepository } from './infra/repositories/transaction-repository';
+import { TransactionRepository } from './infra/repositories';
 import { ITransactionRepository } from './interface';
 import { RegisterTransactionUseCase } from './use-cases/register/register';
 import { WalletRepository } from '@/wallet/infra/repositories';
