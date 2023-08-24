c364b9dd3979e0a2b487ab80c500450a10cfa142
shimarrudz <vic.shima.vss@gmail.com>
2023-08-24 14:50:00 -0300

    feat: add user providers on wallet module

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -6,14 +6,23 @@ import { TransactionController } from '@/shared/infra/http/controllers/transacti
 import { FindTransactionsByUserUseCase } from '@/transaction/use-cases';
 import { ITransactionRepository } from '@/transaction/interface';
 import { TransactionRepository } from '@/transaction/infra/repositories';
+import { IUserRepository } from '@/user/interfaces';
+import { UserRepository } from '@/user/infra/repositories';
+import { FindUserByIdUseCase } from '@/user/use-cases';
+import { User } from '@/user/infra/entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction])],
+  imports: [TypeOrmModule.forFeature([Transaction, User])],
   providers: [
     FindTransactionsByUserUseCase,
+    FindUserByIdUseCase,
     {
       provide: ITransactionRepository,
       useClass: TransactionRepository,
+    },
+    {
+      provide: IUserRepository,
+      useClass: UserRepository,
     },
   ],
   exports: [
