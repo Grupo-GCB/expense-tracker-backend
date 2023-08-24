18d1ef088d6c7dfd87fef237bef9b7517596fadb
shimarrudz <vic.shima.vss@gmail.com>
2023-08-24 02:59:05 -0300

    chore: fix imports

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -5,7 +5,7 @@ import { Transaction } from '@/transaction/infra/entities';
 import { TransactionController } from '@/shared/infra/http/controllers/transaction/transaction.controller';
 import { FindTransactionsByUserUseCase } from '@/transaction/use-cases';
 import { ITransactionRepository } from '@/transaction/interfaces';
-import { TransactionRepository } from './infra/repositories';
+import { TransactionRepository } from '@/transaction/infra/repositories';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Transaction])],
