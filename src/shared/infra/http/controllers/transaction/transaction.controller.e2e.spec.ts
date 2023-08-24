bd391de884de31ccd48a5f696c3cc13c463307e7
shimarrudz <vic.shima.vss@gmail.com>
2023-08-24 03:34:58 -0300

    feat: create e2e tests

---
+++ src/shared/infra/http/controllers/transaction/transaction.controller.e2e.spec.ts
@@ -0,0 +1,95 @@
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+
+import { AppModule } from '@/app.module';
+import { ITransactionRepository } from '@/transaction/interfaces';
+import { Transaction } from '@/transaction/infra/entities';
+import { Categories, TransactionType } from '@/shared/constants';
+
+describe('Transaction Controller (E2E)', () => {
+  let app: INestApplication;
+  let validUserId: string;
+  let testModule: TestingModule;
+  let transactionRepository: ITransactionRepository;
+  let findAllByUserIdMock: jest.SpyInstance;
+
+  beforeAll(async () => {
+    testModule = await Test.createTestingModule({
+      imports: [AppModule],
+      providers: [
+        {
+          provide: ITransactionRepository,
+          useValue: {
+            findAllByUserId: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    transactionRepository = testModule.get<ITransactionRepository>(
+      ITransactionRepository,
+    );
+    findAllByUserIdMock = jest.spyOn(transactionRepository, 'findAllByUserId');
+
+    app = testModule.createNestApplication();
+    await app.init();
+
+    validUserId = 'auth0|user-id';
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  const createMockTransaction = (): Transaction => ({
+    id: '01',
+    categories: Categories.CLOTHES,
+    description: 'Sample Transaction 1',
+    value: 100.0,
+    type: TransactionType.EXPENSE,
+    date: new Date(),
+    created_at: new Date(),
+    updated_at: new Date(),
+    deleted_at: null,
+    wallet: null,
+  });
+
+  const serializeTransaction = (transaction: Transaction): any => ({
+    ...transaction,
+    date: transaction.date.toISOString(), //
+    created_at: transaction.created_at.toISOString(), //
+    updated_at: transaction.updated_at.toISOString(), //
+  });
+
+  describe('/transaction/:user_id (GET)', () => {
+    it('should be defined', () => {
+      expect(transactionRepository).toBeDefined();
+      expect(findAllByUserIdMock).toBeDefined();
+    });
+
+    it('should be able to return transactions for a user', async () => {
+      const mockTransactions: Transaction[] = [createMockTransaction()];
+
+      findAllByUserIdMock.mockResolvedValue(mockTransactions);
+
+      const response = await request(app.getHttpServer())
+        .get(`/transaction/${validUserId}`)
+        .expect(HttpStatus.OK);
+
+      const expectedResponse = mockTransactions.map(serializeTransaction);
+
+      expect(response.body).toEqual(expectedResponse);
+    });
+
+    it('should be able to return an empty list of transactions', async () => {
+      findAllByUserIdMock.mockResolvedValue([]);
+
+      const response = await request(app.getHttpServer())
+        .get(`/transaction/${validUserId}`)
+        .expect(HttpStatus.OK);
+
+      expect(response.body).toEqual([]);
+    });
+  });
+});
