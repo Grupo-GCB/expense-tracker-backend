b99f5af1e89f5256b9ea2fbfd1d9e83e98a61adb
shimarrudz <vic.shima.vss@gmail.com>
2023-08-24 02:58:27 -0300

    chore: fix imports

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -3,9 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Transaction } from '@/transaction/infra/entities';
 import { TransactionController } from '@/shared/infra/http/controllers/transaction/transaction.controller';
-import { FindTransactionsByUserUseCase } from './use-cases';
-import { ITransactionRepository } from './interfaces';
-import { TransactionRepository } from './infra/repositories/transaction.repository';
+import { FindTransactionsByUserUseCase } from '@/transaction/use-cases';
+import { ITransactionRepository } from '@/transaction/interfaces';
+import { TransactionRepository } from './infra/repositories';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Transaction])],
