bf68bd5eac4299ccc26e0aa5272b1822ffe78225
Diogo-gallina <diogogallina20@gmail.com>
2023-08-25 00:39:40 -0300

    feat: create column numeric transformer class

---
+++ src/shared/transformers/numeric-convert.ts
@@ -0,0 +1,16 @@
+import { ValueTransformer } from 'typeorm';
+
+export class ColumnNumericTransformer implements ValueTransformer {
+  private decimalPlaces?: number;
+
+  constructor(decimalPlaces?: number) {
+    this.decimalPlaces = decimalPlaces;
+  }
+
+  to(data: number): number {
+    return data;
+  }
+  from(data: string): number {
+    return parseFloat(data);
+  }
+}
