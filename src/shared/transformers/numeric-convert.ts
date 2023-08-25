4558e7e7032f1df64232c00f1f77d1cd153c28b8
Diogo-gallina <diogogallina20@gmail.com>
2023-08-25 11:59:27 -0300

    chore: skip line

--- src/shared/transformers/numeric-convert.ts
+++ src/shared/transformers/numeric-convert.ts
@@ -10,6 +10,7 @@ export class ColumnNumericTransformer implements ValueTransformer {
   to(data: number): number {
     return data;
   }
+
   from(data: string): number {
     return parseFloat(data);
   }
