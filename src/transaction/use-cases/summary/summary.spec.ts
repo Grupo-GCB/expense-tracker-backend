a8224abac5ff323f00e7abdaf24fcec2c717e96f
shimarrudz <vic.shima.vss@gmail.com>
2023-08-26 12:51:41 -0300

    test: fisrt test correction

--- src/transaction/use-cases/summary/summary.spec.ts
+++ src/transaction/use-cases/summary/summary.spec.ts
@@ -5,6 +5,7 @@ import {
 } from '@/transaction/interface';
 import { Transaction } from '@/transaction/infra/entities';
 import { Categories, TransactionType } from '@/shared/constants';
+import { NotFoundException } from '@nestjs/common';
 
 describe('Find All Transactions By Wallet ID', () => {
   let sut: FindAllByWalletIdUseCase;
@@ -25,13 +26,24 @@ describe('Find All Transactions By Wallet ID', () => {
 
   const validWalletId = '12345';
 
-  const transaction: Transaction[] = [
+  const transactions: Transaction[] = [
     {
       id: '01',
       categories: Categories.CLOTHES,
       description: 'Transaction 1',
       value: 100.0,
       type: TransactionType.INCOME,
+      date: new Date(),
+      created_at: new Date(),
+      updated_at: new Date(),
+      wallet: null,
+    },
+    {
+      id: '02',
+      categories: Categories.FOOD,
+      description: 'Transaction 2',
+      value: 50.0,
+      type: TransactionType.EXPENSE,
       date: new Date(),
       created_at: new Date(),
       updated_at: new Date(),
@@ -45,23 +57,25 @@ describe('Find All Transactions By Wallet ID', () => {
   });
 
   it('should return transactions and balance if wallet is found', async () => {
-    const transactions = [transaction, transaction];
-
-    findAllByWalletIdMock.mockResolvedValue(transactions);
+    findAllByWalletIdMock.mockResolvedValue({
+      transactions,
+    });
 
     const result: ISummaryResponse = await sut.execute(validWalletId);
 
     expect(result.transactions).toEqual(expect.arrayContaining(transactions));
-    expect(result.balance).toBe(50.0);
+    expect(result.balance).toBe(150.0);
 
     expect(findAllByWalletIdMock).toHaveBeenCalledWith(validWalletId);
   });
 
   it('should throw NotFoundException if wallet is not found', async () => {
-    findAllByWalletIdMock.mockResolvedValue([]);
+    findAllByWalletIdMock.mockResolvedValue({
+      transactions: [],
+    });
 
     await expect(sut.execute(validWalletId)).rejects.toThrowError(
-      'Wallet with ID 12345 not found',
+      NotFoundException,
     );
 
     expect(findAllByWalletIdMock).toHaveBeenCalledWith(validWalletId);
